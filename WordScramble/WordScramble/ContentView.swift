90ea9cf1788aa54818375ec4c3f91ab7578252b0
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-06-24 22:14:10 -0700

    first step forward.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Type your new word", text: $newWord)
+                List(usedWords, id: \.self) {
+                    Text($0)
+                    
+                }
+                
+            }
+            .navigationBarTitle(rootWord)
+        }
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

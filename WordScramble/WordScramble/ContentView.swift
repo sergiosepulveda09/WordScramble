8d086762a744e58dba4591a76b7038fb7b5c6e49
Sergio <sergiosepulveda09@live.com>
2021-06-27 13:37:56 -0700

    Challenges added! Application finished

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -14,19 +14,25 @@ struct ContentView: View {
     @State private var errorTitle: String = ""
     @State private var errorMessage: String = ""
     @State private var showingError: Bool = false
-    
+    @State private var scoreCounter: Int = 0
     var body: some View {
         NavigationView {
             VStack {
                 TextField("Type your new word", text: $newWord, onCommit: addNewWord)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)                    
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
                     .padding()
                 List(usedWords, id: \.self) {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                     
                 }
+                HStack {
+                    Text("Words found: ")
+                    Text("\(scoreCounter)")
+                }
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
                 
             }
             .navigationBarTitle(rootWord)
@@ -35,6 +41,19 @@ struct ContentView: View {
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             }
         }
+        .overlay(
+            Button(action: {
+                startGame()
+                usedWords.removeAll()
+                newWord = ""
+            }, label: {
+                Image(systemName: "arrow.counterclockwise")
+            })
+            .padding()
+            .foregroundColor(.black)
+            ,alignment: .topLeading
+        )
+    
         
     }
     func addNewWord() {
@@ -52,11 +71,26 @@ struct ContentView: View {
             return
         }
         guard isReal(word: answer) else {
-            wordError(title: "Word not possible", message: "That isn't a real word")
+            var title: String = ""
+            var message: String = ""
+            if answer.count < 3 {
+                title = "Word is too short"
+                message = "The word should have three or more characters"
+            } else if answer == rootWord {
+                title = "That word is the title!"
+                message =  "Please use a different one"
+                
+            } else {
+                title = "Word not possible"
+                message = "That isn't a real word"
+            }
+            
+            wordError(title: title, message: message)
             return
         }
         
         usedWords.insert(answer, at: 0)
+        scoreCounter += 1
         newWord = ""
     }
     
@@ -90,6 +124,9 @@ struct ContentView: View {
     }
     
     func isReal(word: String) -> Bool {
+        if word.count < 3 || word == rootWord {
+            return false
+        }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
